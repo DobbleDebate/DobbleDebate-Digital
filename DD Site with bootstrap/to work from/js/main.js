bd9279573414d254590fdc280ec708fa622cb221
lhellercnd <contact@lynneheller.com>
2022-02-22 13:19:00 -0500

    Lynne's bootstrap rework
    first pages that were plain html redone with bootstrap

---
+++ DD Site with bootstrap/to work from/js/main.js
@@ -0,0 +1,220 @@
+//   video
+let videoFile1 = document.getElementById('video-file1')
+let videoButton1 = document.getElementById('video__button1')
+let videoButtonIcon1 = document.getElementById('video__icon1')
+
+function playPause1() {
+    if (videoFile1.paused) {
+        videoFile1.play()
+        videoButtonIcon1.classList.add('ri-pause-circle-fill')
+        videoButtonIcon1.classList.remove('ri-play-fill')
+    } else {
+        videoFile1.pause()
+        videoButtonIcon1.classList.add('ri-play-fill')
+        videoButtonIcon1.classList.remove('ri-pause-circle-fill')
+    }
+}
+videoButton1.addEventListener('click', playPause1)
+
+function finalVideo1() {
+    videoButtonIcon1.classList.remove('ri-pause-circle-fill')
+    videoButtonIcon1.classList.add('ri-play-fill')
+}
+videoFile1.addEventListener('ended', finalVideo1)
+
+//   video
+videoFile2 = document.getElementById('video-file2')
+videoButton2 = document.getElementById('video__button2')
+videoButtonIcon2 = document.getElementById('video__icon2')
+
+function playPause2() {
+    if (videoFile2.paused) {
+        videoFile2.play()
+        videoButtonIcon2.classList.add('ri-pause-circle-fill')
+        videoButtonIcon2.classList.remove('ri-play-fill')
+    } else {
+        videoFile2.pause()
+        videoButtonIcon2.classList.add('ri-play-fill')
+        videoButtonIcon2.classList.remove('ri-pause-circle-fill')
+    }
+}
+videoButton2.addEventListener('click', playPause2)
+
+function finalVideo2() {
+    videoButtonIcon2.classList.remove('ri-pause-circle-fill')
+    videoButtonIcon2.classList.add('ri-play-fill')
+}
+videoFile2.addEventListener('ended', finalVideo2)
+
+
+//   video
+videoFile3 = document.getElementById('video-file3')
+videoButton3 = document.getElementById('video__button3')
+videoButtonIcon3 = document.getElementById('video__icon3')
+
+function playPause3() {
+    if (videoFile3.paused) {
+        videoFile3.play()
+        videoButtonIcon3.classList.add('ri-pause-circle-fill')
+        videoButtonIcon3.classList.remove('ri-play-fill')
+    } else {
+        videoFile3.pause()
+        videoButtonIcon3.classList.add('ri-play-fill')
+        videoButtonIcon3.classList.remove('ri-pause-circle-fill')
+    }
+}
+videoButton3.addEventListener('click', playPause3)
+
+function finalVideo3() {
+    videoButtonIcon3.classList.remove('ri-pause-circle-fill')
+    videoButtonIcon3.classList.add('ri-play-fill')
+}
+videoFile3.addEventListener('ended', finalVideo3)
+
+
+//   video
+videoFile4 = document.getElementById('video-file4')
+videoButton4 = document.getElementById('video__button4')
+videoButtonIcon4 = document.getElementById('video__icon4')
+
+function playPause4() {
+    if (videoFile4.paused) {
+        videoFile4.play()
+        videoButtonIcon4.classList.add('ri-pause-circle-fill')
+        videoButtonIcon4.classList.remove('ri-play-fill')
+    } else {
+        videoFile4.pause()
+        videoButtonIcon4.classList.add('ri-play-fill')
+        videoButtonIcon4.classList.remove('ri-pause-circle-fill')
+    }
+}
+videoButton4.addEventListener('click', playPause4)
+
+function finalVideo4() {
+    videoButtonIcon4.classList.remove('ri-pause-circle-fill')
+    videoButtonIcon4.classList.add('ri-play-fill')
+}
+videoFile4.addEventListener('ended', finalVideo4)
+
+//   video
+videoFile5 = document.getElementById('video-file5')
+videoButton5 = document.getElementById('video__button5')
+videoButtonIcon5 = document.getElementById('video__icon5')
+
+function playPause5() {
+    if (videoFile5.paused) {
+        videoFile5.play()
+        videoButtonIcon5.classList.add('ri-pause-circle-fill')
+        videoButtonIcon5.classList.remove('ri-play-fill')
+    } else {
+        videoFile5.pause()
+        videoButtonIcon5.classList.add('ri-play-fill')
+        videoButtonIcon5.classList.remove('ri-pause-circle-fill')
+    }
+}
+videoButton5.addEventListener('click', playPause5)
+
+function finalVideo5() {
+    videoButtonIcon5.classList.remove('ri-pause-circle-fill')
+    videoButtonIcon5.classList.add('ri-play-fill')
+}
+videoFile5.addEventListener('ended', finalVideo5)
+
+
+//   video
+videoFile6 = document.getElementById('video-file6')
+videoButton6 = document.getElementById('video__button6')
+videoButtonIcon6 = document.getElementById('video__icon6')
+
+function playPause6() {
+    if (videoFile6.paused) {
+        videoFile6.play()
+        videoButtonIcon6.classList.add('ri-pause-circle-fill')
+        videoButtonIcon6.classList.remove('ri-play-fill')
+    } else {
+        videoFile6.pause()
+        videoButtonIcon6.classList.add('ri-play-fill')
+        videoButtonIcon6.classList.remove('ri-pause-circle-fill')
+    }
+}
+videoButton6.addEventListener('click', playPause6)
+
+function finalVideo6() {
+    videoButtonIcon6.classList.remove('ri-pause-circle-fill')
+    videoButtonIcon6.classList.add('ri-play-fill')
+}
+videoFile6.addEventListener('ended', finalVideo6)
+
+
+//   video
+videoFile7 = document.getElementById('video-file7')
+videoButton7 = document.getElementById('video__button7')
+videoButtonIcon7 = document.getElementById('video__icon7')
+
+function playPause7() {
+    if (videoFile7.paused) {
+        videoFile7.play()
+        videoButtonIcon7.classList.add('ri-pause-circle-fill')
+        videoButtonIcon7.classList.remove('ri-play-fill')
+    } else {
+        videoFile7.pause()
+        videoButtonIcon7.classList.add('ri-play-fill')
+        videoButtonIcon7.classList.remove('ri-pause-circle-fill')
+    }
+}
+videoButton7.addEventListener('click', playPause7)
+
+function finalVideo7() {
+    videoButtonIcon7.classList.remove('ri-pause-circle-fill')
+    videoButtonIcon7.classList.add('ri-play-fill')
+}
+videoFile7.addEventListener('ended', finalVideo7)
+
+
+//   video
+videoFile8 = document.getElementById('video-file8')
+videoButton8 = document.getElementById('video__button8')
+videoButtonIcon8 = document.getElementById('video__icon8')
+
+function playPause8() {
+    if (videoFile8.paused) {
+        videoFile8.play()
+        videoButtonIcon8.classList.add('ri-pause-circle-fill')
+        videoButtonIcon8.classList.remove('ri-play-fill')
+    } else {
+        videoFile8.pause()
+        videoButtonIcon8.classList.add('ri-play-fill')
+        videoButtonIcon8.classList.remove('ri-pause-circle-fill')
+    }
+}
+videoButton8.addEventListener('click', playPause8)
+
+function finalVideo8() {
+    videoButtonIcon8.classList.remove('ri-pause-circle-fill')
+    videoButtonIcon8.classList.add('ri-play-fill')
+}
+videoFile8.addEventListener('ended', finalVideo8)
+
+//   video
+videoFile9 = document.getElementById('video-file9')
+videoButton9 = document.getElementById('video__button9')
+videoButtonIcon9 = document.getElementById('video__icon9')
+
+function playPause9() {
+    if (videoFile9.paused) {
+        videoFile9.play()
+        videoButtonIcon9.classList.add('ri-pause-circle-fill')
+        videoButtonIcon9.classList.remove('ri-play-fill')
+    } else {
+        videoFile9.pause()
+        videoButtonIcon9.classList.add('ri-play-fill')
+        videoButtonIcon9.classList.remove('ri-pause-circle-fill')
+    }
+}
+videoButton9.addEventListener('click', playPause9)
+
+function finalVideo9() {
+    videoButtonIcon9.classList.remove('ri-pause-circle-fill')
+    videoButtonIcon9.classList.add('ri-play-fill')
+}
+videoFile9.addEventListener('ended', finalVideo9)
